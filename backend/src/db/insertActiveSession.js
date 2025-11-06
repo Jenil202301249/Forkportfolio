92e4ec6b8ecf373f5c5f1e2cfc9c22e1d0d7282b
Jenil202301249 <202301249@dau.ac.in>
2025-11-06 07:51:40 +0530

    fix: Improve error logging in insertActiveSession function

--- backend/src/db/insertActiveSession.js
+++ backend/src/db/insertActiveSession.js
@@ -5,7 +5,7 @@ const insertActiveSession = async ({ token, email, browser_type, os_type}) => {
         const result = await sql`INSERT INTO "active_session" (token,email,browser_type,os_type) VALUES (${token},${email},${browser_type},${os_type})RETURNING email`;
         return result; 
     } catch (error) {
-        console.log('Database Error - insertActiveSession');
+        console.log('Database Error - insertActiveSession ',error);
         return null;
     }
 }
