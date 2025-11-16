e5f4212a59f75d25bc816f579c11f1859d9d31ef
Jenil202301249 <202301249@dau.ac.in>
2025-11-16 15:07:32 +0530

    fix: standardize error logging in insertActiveSession function

--- backend/src/db/insertActiveSession.js
+++ backend/src/db/insertActiveSession.js
@@ -5,11 +5,7 @@ const insertActiveSession = async ({ token, email, browser_type, os_type}) => {
         const result = await sql`INSERT INTO "active_session" (token,email,browser_type,os_type) VALUES (${token},${email},${browser_type},${os_type})RETURNING email`;
         return result; 
     } catch (error) {
-<<<<<<< HEAD
-        console.log('Database Error - insertActiveSession ',error);
-=======
         console.error('Database Error - insertActiveSession',error);
->>>>>>> 2ed7a892d6312e1554e55b060f39dcc9971f8822
         return null;
     }
 }
